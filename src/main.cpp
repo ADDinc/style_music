75a0f4d2aa922c880beb7c3e095bff2844c26dac
Denis Dyachkov <denisrafdyachkov@yandex.ru>
2017-05-19 19:48:39 +0700

    Add argument keys -f, -i.

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,46 @@
 #include "filesystem.hpp"
-#include <unistd.h>
 #include "NeuronNetwork.hpp"
 
 using namespace std;
 using namespace essentia;
 using namespace essentia::standard;
 
+bool learn = false;
+bool info = false;
+string style;
+
 int main(int argc, char *argv[])
 {
-    if (argc == 1) {
-        cout << "Use: " << argv[0] << " <filename|-d (directory)|-f (file)> <необязательные параметры>" << endl;
-        return 1;
-    }
-
-    int rez = 0;
-    string dir, style;
-    bool learn = false;
-    while ((rez = getopt(argc, argv, "d:l:")) != -1) {
-        switch (rez) {
-        case 'd':
-            printf("found argument \"d = %s\".\n", optarg);
-            dir = optarg;
-            break;
-        case 'l':
-            printf("found argument \"l = %s\".\n", optarg);
-            style = optarg;
-            stringTolower(style);
-            learn = true;
-            break;
-        case '?':
-            printf("Error found !\n");
-            break;
-        };
-    };
-    essentia::init();
     vector <string> files;
+    if (input(argc, argv, files) == -1)
+        return -1;
+    if (!files.empty()) {
+        essentia::init();
 
-    try {
-        vector <string> mask = getRegexMask("*.flac|*.mp3|*.m4a");
-        getFileList(dir, files, mask);
-    }
-    catch (exception &ex) {
-        exceptionPrint(ex, "main (get file list)");
-    }
-    MapTags v2;
-    MapMono v;
+        MapTags v2;
+        MapMono v;
 
-    try {
-        NeuronNetwork neuNtw("NN.db"); // загрузка
-        vector<double> power;
-        for (auto &f : files) {
-            v2 = getMapMetadataReader(getPoolMetadataReader(f));
-            v = getMapMonoLoader(getPoolMonoLoader(f));
-            if (learn) {
-                neuNtw.learning(style, v);
-            } else {
-                neuNtw.printPower(v);
+        try {
+            NeuronNetwork neuNtw("NN.db"); // загрузка
+            vector<double> power;
+            for (auto &f : files) {
+                v2 = getMapMetadataReader(getPoolMetadataReader(f));
+                v = getMapMonoLoader(getPoolMonoLoader(f));
+                if (learn) {
+                    neuNtw.learning(style, v);
+                } else {
+                    neuNtw.printPower(v);
+                }
             }
         }
-    }
-    catch (exception &ex) {
-        exceptionPrint(ex, "main (learning)");
-    }
+        catch (exception &ex) {
+            exceptionPrint(ex, "main (learning)");
+        }
 
-    essentia::shutdown();
+        essentia::shutdown();
+    } else {
+        cerr << "File list is empty!" << endl << "Use: " << argv[0] << " <filename|-d (directory)|-f (file)> <необязательные параметры>" << endl;
+        return 1;
+    }
     return 0;
 }
